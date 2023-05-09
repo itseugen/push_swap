452e865ea66e3367d8c5919742d46eb8a588597d
eweiberl <eweiberl@student.42heilbronn.de>
2023-05-09 16:20:23 +0200

    deleted push_ab

--- stackops.c
+++ stackops.c
@@ -6,33 +6,14 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 13:37:08 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/05/09 13:52:55 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/05/09 16:19:34 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	push_ab(t_stack *stackA, t_stack *stackB, char *id)
+int	push_ab(t_stack *stackA, t_stack *stackB, char id)
 {
-	int	val;
-
-	if (id == "pa")
-	{
-		val = pop(&stackB);
-		if (val == 0)
-			return (0); //DO NOTHING
-		if (push(&stackA, val) == ALLOC_FAIL)
-			return (ALLOC_FAIL); //!ADD EXIt fUNCtion HERE
-		return (9);
-	}
-	if (id == "pa")
-	{
-		val = pop(&stackA);
-		if (val == 0)
-			return (0); //DO NOTHING
-		if (push(&stackB, val) == ALLOC_FAIL)
-			return (ALLOC_FAIL); //!ADD EXIt fUNCtion HERE
-		return (10);
-	}
+	
 }
 //!Think about just changing pointers -> less systemcalls and memory leaks
