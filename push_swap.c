6c197c4aae3e0f76982a1ddd6b71b2cd15079dda
eweiberl <eweiberl@student.42heilbronn.de>
2023-07-18 12:42:34 +0200

    norm conformity

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 12:49:42 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/07/18 10:59:56 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/07/18 12:42:22 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,6 +78,5 @@ int	main(int argc, char *argv[])
 	sorting(&stack1, &stack2);
 	free_list(&stack1);
 	free_list(&stack2);
-	system("leaks push_swap");
 	return (FINISHED);
 }
